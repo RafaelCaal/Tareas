172c74176bcbb2334e1d6c9c83a383865acbf159
Rafael Caal <rafael.caal@gmail.com>
2022-02-06 16:22:48 -0600

    first commit

---
+++ Actividad2.js
@@ -0,0 +1,13 @@
+var mes = 10
+
+if (mes <= 2 && mes==12){
+  console.log("Es invierno")
+} else if (mes>=3 && mes<=5){
+  console.log("Es primavera")
+} else if (mes>=6 && mes<=8){
+  console.log("Es verano")
+} else if (mes>=9 && mes <=11){
+  console.log("Es otono")
+}
+
+console.log("22005973 - Rafael Angel Caal Tzul")
